4e846bc9c9eda85e587b8b7429414b4aa96673b6
HerrLeStrate <vemakus@gmail.com>
2019-02-14 13:40:18 +0300

    Third task

---
+++ Task3/PTaT_Lab2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTaT_Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            StringBuilder stringer = new StringBuilder(input);
+            for(int i = 0; i < input.Length; i++)
+            {
+                if (stringer[i] == ' ') continue;
+                stringer[i] = (stringer[i] == 'z') ? stringer[i] = 'a' : stringer[i] = (char)((int)(stringer[i]) + 1);
+            }
+            input = stringer.ToString();
+            Console.WriteLine(input);
+            Console.Read();
+        }
+    }
+}
